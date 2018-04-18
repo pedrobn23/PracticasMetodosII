5cbeabccc14c0016b5e822942c847c83175a0948
pedrobn23 <pedrobonilla@correo.ugr.es>
2018-04-19 01:38:15 +0200

    Hay que arreglar el metodo para rutilizar las integrales

--- P2/ejercicio5.py
+++ P2/ejercicio5.py
@@ -4,38 +4,53 @@ from numpy.polynomial import polynomial as P
 
 
 def intTrapecio( f, a, b, n ):
-     h = (b-a)/n
-     x=[ a+h*i for i in range(n+1)]
-     
      if(n==0):
           resultado = (f(b) + f(a))*(b-a)/2
      else:
+          h = (b-a)/n
+          x=[ a+h*i for i in range(n+1)]
           resultado = ( h *( ( f(x[0]) + f(x[n]) )/2 + sum([f(x[i+1]) for i in range(n-1)]) ) )
 
      return resultado
+
 
 def ampliaIntegral( f, a, b, Rk, k):
      index = 2**(k)
      h = (b-a)/(index * 2)
      resultado=0
-     
-          resultado= Rk/2 + ( sum( [f(a +(2i+1)h) for i in range(index)] ) )
+     resultado= Rk/2 + ( sum( [f(a +(2*i+1)*h) for i in range(index)] ) )
 
      return resultado
 
+
 def rombergParcial( Rk, Rk1, j ):
      return Rk + (1/(4**j-1))*(Rk - Rk1)
-          
+
+
 def romberg(f, a, b, n):
      R=[]
      R.append( intTrapecio( f, a, b, 0 ) )
      for i in range(n):
-          R.append( ampliaIntegral( f, a, b, R[i], i ) )
+          R.append( intTrapecio( f, a, b, 2**n ) )
+
+     #TODO: ampiaIntegral no funciona, revisar.
+     #R.append( ampliaIntegral( f, a, b, R[i], i ) )
 
      for i in range(n):
           for j in range(n-i):
-               
+               R[j]=rombergParcial( R[j+1], R[j], j+1 )
 
+     return R[0]
+
+
+f = lambda x: np.log(x)
+a=1
+b=2
+n=10
+
+resultado = romberg( f, a, b, n )
+print('Resultado increible')
+print(resultado)
 
      
      
