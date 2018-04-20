812af8ddbdc08012ab247d6f671e27214ab4b158
pedrobonilla@correo.ugr.es <pedrobonilla@correo.ugr.es>
2018-04-20 19:18:24 +0200

    Corregido ejercicio 3

--- P2/source/ejercicio3.py
+++ P2/source/ejercicio3.py
@@ -7,8 +7,9 @@ def intTrapecio( f, a, b, n ):
      h = (b-a)/n
      x=[ a+h*i for i in range(n+1)]
 
-     
-     if(n==0):
+     if(n<1):
+          resultado=0     
+     elif(n==1):
           resultado = (f(b) + f(a))*(b-a)/2
      else:
           resultado = ( h *( ( f(x[0]) + f(x[n]) )/2 + sum([f(x[i+1]) for i in range(n-1)]) ) )
