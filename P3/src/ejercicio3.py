9df04ef502141cd347d75576b731ce04d3bfc826
jocaro97 <johanna_capote_robayna@hotmail.com>
2018-05-27 12:35:37 +0200

    comentado

--- P3/src/ejercicio3.py
+++ P3/src/ejercicio3.py
@@ -1,12 +1,33 @@
-f = lambda t,y: -y+t+1
-a = 0
-b = 1
+"""
+Datos de entrada
+"""
+
+# Intervalo [a,b]
+a = 0.0
+b = 1.0
+
+# Número de divisiones del intervalo
 n = 5
-y0= 1
+
+# Número de pasos
+k = 3
+
+# Valor inicial y0 = y(a)
+y0 = 1
+
+# Función f de dos variables
+f = lambda t,y: -y+t+1
 
 h = (b-a) / n
 t = [a + j*h for j in range(n+1)]
 
+'''
+Método de Euler Mejorado
+'''
+# Intervalo (a,b)
+# n número de nodos
+# f f(t, y(t)) = y'
+# y0 valor de y en t_0
 
 def eulerMejorado():
     u = []
